25c44b9c67dee06bc6347c0b2c47eb4b68fe97dd
700hours <31488826+700hours@users.noreply.github.com>
2018-03-06 16:59:38 -0800

    Add files via upload
    Changelog
    - Added Screw Attack usage
    - Proper drawing code with flipping sprites

---
+++ Items/Extensions/ScrewAttack.cs
@@ -0,0 +1,103 @@
+
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using SuperMetroid;
+
+namespace SuperMetroid.Items.Extensions
+{
+	public class ScrewAttack : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Speed Booster");
+			Tooltip.SetDefault("Damages enemies while somersaulting."
+				+ 	"\nIncreased invincibility effect when airborne.");
+		}
+		public override void SetDefaults()
+		{
+			item.width = 28;
+			item.height = 28;
+			item.maxStack = 1;
+			item.value = 100;
+			item.rare = 5;
+			item.scale = 1f;
+			item.accessory = true;
+		}
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(9);
+			recipe.SetResult(this, 1);
+			recipe.AddRecipe();
+		}
+		
+		bool Attacking;
+		int sa;
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+		#region ScrewAttack
+			if(player.controlRight || player.velocity.X > 0 && player.velocity.Y != 0)
+			{
+				if(player.velocity.Y != 0 && player.velocity.X != 0 && player.itemAnimation == 0 && player.releaseHook && player.grapCount == 0/* && !ModPlayer.grappled*/)
+				{
+					Attacking = false;
+				}
+				foreach(Projectile P in Main.projectile)
+				{
+					if(P.active && P.owner==player.whoAmI && P.type == mod.ProjectileType("ScrewAttack"))
+					{
+						Attacking = true;
+						player.invis = true;
+						break;
+					}
+				}		
+				if(!Attacking)
+				{
+					int damage = 150;
+					Projectile.NewProjectile(player.position.X+player.width/2,player.position.Y+player.height/2,0,0,mod.ProjectileType("ScrewAttack"),damage,0,player.whoAmI);
+				}	
+			}
+
+			if(player.controlLeft || player.velocity.X < 0 && player.velocity.Y != 0)
+			{
+				if(player.velocity.Y != 0 && player.velocity.X != 0 && player.itemAnimation == 0 && player.releaseHook && player.grapCount == 0/* && !ModPlayer.grappled*/)
+				{
+					Attacking = false;
+				}
+				foreach(Projectile P in Main.projectile)
+				{
+					if(P.active && P.owner==player.whoAmI && P.type == mod.ProjectileType("ScrewAttack"))
+					{
+						Attacking = true;
+						player.invis = true;
+						break;
+					}
+				}		
+				if(!Attacking)
+				{
+					int damage = 150;
+					Projectile.NewProjectile(player.position.X+player.width/2,player.position.Y+player.height/2,0,0,mod.ProjectileType("ScrewAttack"),damage,0,player.whoAmI);
+				}	
+			}
+		#endregion
+
+			if(Attacking && player.velocity.Y != 0 && player.velocity.X != 0 && player.itemAnimation == 0 && player.releaseHook && player.grapCount == 0/* && !ModPlayer.grappled*/)
+			{
+				player.head = -1;
+				player.body = -1;
+				player.legs = -1;
+				player.wings = 0;	
+			
+				Color color = new Color();
+				int dust = Dust.NewDust(player.position, player.width, player.height, 61, 0, 0, 200, color, 1.2f);
+				Main.dust[dust].noGravity = true;
+				Main.dust[dust].noLight = false;
+			}
+		}
+	}
+}
