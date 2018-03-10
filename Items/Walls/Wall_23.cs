fc0a40c3d87bbc8aa97ec01d95b5d1880d1e05a9
700hours <31488826+700hours@users.noreply.github.com>
2018-03-10 15:19:44 -0800

    Add files via upload
    Changelog
    - Added 26 Wall (items)

---
+++ Items/Walls/Wall_23.cs
@@ -0,0 +1,35 @@
+using Terraria.ModLoader;
+
+namespace SuperMetroid.Items.Walls
+{
+	public class Wall_23 : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Dark Sandstone");
+			Tooltip.SetDefault("");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 12;
+			item.height = 12;
+			item.maxStack = 999;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 7;
+			item.useStyle = 1;
+			item.consumable = false;
+			item.createWall = mod.WallType("Wall_23");
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(9);
+			recipe.SetResult(this, 4);
+			recipe.AddRecipe();
+		}
+	}
+}
